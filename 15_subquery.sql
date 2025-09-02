ed4c0df2f398b569b2fbdcb795f99e252b503777
KarolSalvador <karolsalvador91@gmail.com>
2025-09-02 16:28:41 -0300

    adiciona arquivo 15 de subquery

---
+++ 15_subquery.sql
@@ -0,0 +1,14 @@
+--9. Dos clientes que começaram SQL no primeiro dia, quantos chegaram ao 5o dia?
+
+--leitura: seleciona todas as colunas da tabela transações com filtro dos clientes que tiveram transações dia 29 e clientes que fizeram alguma transação dia 25
+
+SELECT *
+
+FROM transacoes AS t1
+
+WHERE t1.IdCliente IN (
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE substr(DtCriacao, 1, 10) = '2025-08-25'
+)
+AND substr(t1.DtCriacao, 1, 10) = '2025-08-29'
